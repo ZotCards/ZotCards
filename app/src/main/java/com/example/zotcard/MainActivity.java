ec91ab218a9859aaf9dc092a13bfbab16bf88d7b
alexiv2 <alexiv2@.uci.edu>
2021-03-09 18:40:29 -0800

    Connecting activities to xml

--- app/src/main/java/com/example/zotcard/MainActivity.java
+++ app/src/main/java/com/example/zotcard/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.zotcard;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    void mainActivityIntent(){
+        Intent i = new Intent(this,MainActivity.class);
+        startActivity(i);
+    }
+
+    void CreateDeckActivityIntent(){
+        Intent i = new Intent(this,CreateDeckActivity.class);
+        startActivity(i);
     }
 }
